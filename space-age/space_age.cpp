14d8cfcf23e16beb2b536e770d67663f57be5dce
JSanestrzik <j.sanestrzik@gmail.com>
2021-01-29 22:38:05 +0100

    Space age

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -2,4 +2,50 @@
 
 namespace space_age {
 
+    space_age::space_age(long seconds): mSeconds(seconds) {}
+
+    long space_age::seconds() const
+    {
+        return mSeconds;
+    }
+
+    double space_age::on_mercury() const
+    {
+        return  on_earth() / mEarthMercuryFactor;
+    }
+
+    double space_age::on_venus() const
+    {
+        return on_earth() / mEarthVenusFactor;
+    }
+
+    double space_age::on_earth() const
+    {
+        return (double)mSeconds/mEarthYearSeconds;
+    }
+
+    double space_age::on_mars() const
+    {
+        return on_earth() / mEarthMarssFactor;
+    }
+
+    double space_age::on_jupiter() const
+    {
+        return on_earth() / mEarthJupiterFactor;
+    }
+
+    double space_age::on_saturn() const
+    {
+        return on_earth() / mEarthSaturnFactor;
+    }
+
+    double space_age::on_uranus() const
+    {
+        return on_earth() / mEarthUranusFactor;
+    }
+
+    double space_age::on_neptune() const
+    {
+        return on_earth() / mEarthNeptuneFactor;
+    }
 }  // namespace space_age
--- space-age/space_age.h
+++ space-age/space_age.h
@@ -1,8 +1,34 @@
 #if !defined(SPACE_AGE_H)
 #define SPACE_AGE_H
 
-namespace space_age {
+#define EXERCISM_RUN_ALL_TESTS
 
+namespace space_age {
+    class space_age {
+    public:
+        space_age(long seconds);
+
+        long seconds() const;
+        double on_mercury()const;
+        double on_venus() const;
+        double on_earth() const;
+        double on_mars() const;
+        double on_jupiter() const;
+        double on_saturn() const;
+        double on_uranus() const;
+        double on_neptune() const;
+    private:
+        long mSeconds;
+
+        const double mEarthYearSeconds = 31557600;
+        const double mEarthMarssFactor = 1.8808158;
+        const double mEarthMercuryFactor = 0.2408467;
+        const double mEarthVenusFactor = 0.61519726;
+        const double mEarthJupiterFactor = 11.862615;
+        const double mEarthSaturnFactor = 29.447498;
+        const double mEarthUranusFactor = 84.016846;
+        const double mEarthNeptuneFactor = 164.79132;
+    };
 }  // namespace space_age
 
-#endif // SPACE_AGE_H
+#endif // SP    ACE_AGE_H
