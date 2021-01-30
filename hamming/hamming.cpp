06026613389b6a44059f15c320a70a573be86bd2
JSanestrzik <j.sanestrzik@gmail.com>
2021-01-30 23:54:39 +0100

    Solve hamming exercise

--- hamming/hamming.cpp
+++ hamming/hamming.cpp
@@ -1,5 +1,26 @@
 #include "hamming.h"
 
+#include <iostream>
+
 namespace hamming {
 
+    int compute(std::string lhs, std::string rhs) throw(std::domain_error)
+    {
+        if (lhs.length() != rhs.length())
+        {
+            throw std::domain_error("DNA strands differ in length");
+        }
+
+        int hammingDst = 0;
+
+        for (auto it = lhs.begin(); it < lhs.end(); it++)
+        {
+            if (*it != rhs[std::distance(lhs.begin(), it)])
+            {
+                hammingDst++;
+            }
+        }
+
+        return hammingDst;
+    }
 }  // namespace hamming
--- hamming/hamming.h
+++ hamming/hamming.h
@@ -1,8 +1,9 @@
 #if !defined(HAMMING_H)
 #define HAMMING_H
+#include <string>
 
 namespace hamming {
-
+    int compute(std::string lhs, std::string rhs) throw(std::domain_error);
 }  // namespace hamming
 
 #endif // HAMMING_H
