c5801ca78a31603d8a8e5812fe8b77c9d17f843c
JSanestrzik <j.sanestrzik@gmail.com>
2021-02-02 21:54:37 +0100

    Add triangle exercise solution

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,5 +1,40 @@
 #include "triangle.h"
 
-namespace triangle {
+#include <map>
+#include <array>
+#include <algorithm>
+#include <numeric>
 
+#include <iostream>
+
+namespace triangle {
+    flavor kind(double a, double b, double c) {
+        std::array<double, 3> values{a, b, c};
+        std::sort(values.begin(), values.end());
+
+        if (std::find_if(values.begin(), values.end(),[](double v){ return v <= 0; }) != values.end()) {
+            throw std::domain_error("Triangle side can't be zero or negative");
+        }
+
+        double shortSidesSum = std::accumulate(values.begin(), values.end() -1, 0.0);
+        double longSide = values.back();
+
+        if (shortSidesSum < longSide) {
+            throw std::domain_error("Invalid triangle side values");
+        }
+
+        std::map<double, int> sides;
+        for (double val : values) {
+            sides[val]++;
+        }
+
+        switch (sides.size()) {
+            case 3:
+                return flavor::scalene;
+            case 2:
+                return flavor::isosceles;
+            default:
+                return flavor::equilateral;
+        }
+    }
 }  // namespace triangle
--- triangle/triangle.h
+++ triangle/triangle.h
@@ -1,8 +1,17 @@
 #if !defined(TRIANGLE_H)
 #define TRIANGLE_H
 
+#define EXERCISM_RUN_ALL_TESTS
+
 namespace triangle {
 
+    enum class flavor {
+        equilateral,
+        isosceles,
+        scalene
+    };
+
+    flavor kind(double a, double b, double c);
 }  // namespace triangle
 
 #endif // TRIANGLE_H
