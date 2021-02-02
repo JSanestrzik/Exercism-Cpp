48b5f01931607dd2473c71adf26b77d9ef91b343
JSanestrzik <j.sanestrzik@gmail.com>
2021-02-02 22:53:37 +0100

    Add grade school work in progress

--- grade-school/grade_school.cpp
+++ grade-school/grade_school.cpp
@@ -2,4 +2,7 @@
 
 namespace grade_school {
 
+    std::vector<std::string> school::roster() const {
+        return std::vector<std::string>();
+    }
 }  // namespace grade_school
--- grade-school/grade_school.h
+++ grade-school/grade_school.h
@@ -1,8 +1,20 @@
 #if !defined(GRADE_SCHOOL_H)
 #define GRADE_SCHOOL_H
 
+#include <vector>
+#include <string>
+
+#define EXERCISM_RUN_ALL_TESTS
+
 namespace grade_school {
 
+    class school {
+    public:
+        school() = default;
+
+        void add(std::string name, int grade);
+        std::vector<std::string> roster() const;
+    };
 }  // namespace grade_school
 
 #endif // GRADE_SCHOOL_H
