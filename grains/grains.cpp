b4783c1fc4b3c1139318991c1b1ed439848245a3
JSanestrzik <j.sanestrzik@gmail.com>
2021-02-11 23:24:04 +0100

    Add solution to grains exercise

--- grains/grains.cpp
+++ grains/grains.cpp
@@ -1,5 +1,30 @@
 #include "grains.h"
 
+#include <map>
+#include <stdexcept>
+
 namespace grains {
+
+    unsigned long long square(unsigned field_number) {
+        static unsigned long long squares[64] = { 1ULL };
+        if (field_number == 0) {
+            throw std::invalid_argument("Field number can't be equal to 0");
+        }
+        if (squares[field_number - 1] != 0) {
+            return squares[field_number - 1];
+        } else {
+            unsigned long long grains = square(field_number - 1) * 2ULL;
+            squares[field_number -1] = grains;
+            return grains;
+        }
+    }
+
+    unsigned long long total() {
+        unsigned long long sum = 0;
+        for (int i = 1; i <= 64; i++) {
+            sum += square(i);
+        }
+        return sum;
+    }
 
 }  // namespace grains
--- grains/grains.h
+++ grains/grains.h
@@ -1,7 +1,15 @@
 #if !defined(GRAINS_H)
 #define GRAINS_H
 
+#define EXERCISM_RUN_ALL_TESTS
+
+#include <map>
+#include <stdexcept>
+
 namespace grains {
+
+    unsigned long long square(unsigned field_number);
+    unsigned long long total();
 
 }  // namespace grains
 
