8b9d8e85da64826af5d321961fdc4b249f935ed3
JSanestrzik <j.sanestrzik@gmail.com>
2021-02-17 20:28:07 +0100

    Add solution to sum of squares exercise

--- difference-of-squares/difference_of_squares.cpp
+++ difference-of-squares/difference_of_squares.cpp
@@ -1,5 +1,16 @@
 #include "difference_of_squares.h"
+#include <cmath>
 
 namespace difference_of_squares {
+    int square_of_sum(int square) {
+        return pow( ((2 + (square -1)) / 2) * square , 2);
+    }
 
+    int sum_of_squares(int square) {
+        return (square * (square + 1) * (2 * square + 1)) / 6;
+    }
+
+    int difference(int square) {
+        return square_of_sum(square) - sum_of_squares(square);
+    }
 }  // namespace difference_of_squares
--- difference-of-squares/difference_of_squares.h
+++ difference-of-squares/difference_of_squares.h
@@ -2,7 +2,9 @@
 #define DIFFERENCE_OF_SQUARES_H
 
 namespace difference_of_squares {
-
+    int square_of_sum(int square);
+    int sum_of_squares(int square);
+    int difference(int square);
 }  // namespace difference_of_squares
 
 #endif // DIFFERENCE_OF_SQUARES_H
