d2241e91b41aebaeac22dad870de18fe521e8dbd
JSanestrzik <j.sanestrzik@gmail.com>
2021-02-17 21:24:35 +0100

    Add solution for raindrops eercise

--- raindrops/raindrops.cpp
+++ raindrops/raindrops.cpp
@@ -2,4 +2,24 @@
 
 namespace raindrops {
 
+    static const char FACTOR_3[] = "Pling";
+    static const char FACTOR_5[] = "Plang";
+    static const char FACTOR_7[] = "Plong";
+
+    std::string convert(int drops) {
+        std::string result;
+        if (drops % 3 == 0) {
+            result += FACTOR_3;
+        }
+        if (drops % 5 == 0) {
+            result += FACTOR_5;
+        }
+        if (drops % 7 == 0) {
+            result += FACTOR_7;
+        }
+        if (result.empty()) {
+            result += std::to_string(drops);
+        }
+        return result;
+    }
 }  // namespace raindrops
--- raindrops/raindrops.h
+++ raindrops/raindrops.h
@@ -1,8 +1,9 @@
 #if !defined(RAINDROPS_H)
 #define RAINDROPS_H
-
+#include <string>
+#define EXERCISM_RUN_ALL_TESTS
 namespace raindrops {
-
+    std::string convert(int drops);
 }  // namespace raindrops
 
 #endif // RAINDROPS_H
