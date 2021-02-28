bd310358c5ce3af35cbd40518311f7ef308bb3c0
Jakub Sanestrzik <j.sanestrzik@gmail.com>
2021-02-28 19:29:40 +0100

    Update Pangram solution

--- pangram/pangram.cpp
+++ pangram/pangram.cpp
@@ -5,11 +5,11 @@
 namespace pangram {
 
     bool is_pangram(std::string val, const std::size_t alphabet_count, const std::locale loc) {
-        val.erase(std::remove_if(val.begin(), val.end(), [loc](char c){ return !std::isalpha(c, loc);}), val.end());
         if (val.length() == 0) {
             return false;
         }
 
+        val.erase(std::remove_if(val.begin(), val.end(), [loc](char c){ return !std::isalpha(c, loc);}), val.end());
         std::transform(val.begin(), val.end(), val.begin(), [](char c){ return std::tolower(c); });
         const std::set<char> pangram(val.begin(), val.end());
         return pangram.size() == alphabet_count;
