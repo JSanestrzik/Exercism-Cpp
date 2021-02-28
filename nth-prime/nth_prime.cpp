66422f7ab8c91b62a3fe8f72e0609e66cca92a19
JSanestrzik <j.sanestrzik@gmail.com>
2021-02-28 20:00:26 +0100

    Add solution to Nth prime exercise

--- nth-prime/nth_prime.cpp
+++ nth-prime/nth_prime.cpp
@@ -1,7 +1,26 @@
 #include "nth_prime.h"
+#include <stdexcept>
+#include <vector>
 
 namespace nth_prime {
     int nth(int n) {
-        return n;
+        if (n <= 0) {
+            throw std::domain_error("The n-th prime value can't be 0 or less");
+        }
+
+        std::vector<int> primes = { 2 };
+        int val = primes.back();
+        while ((int)primes.size() < n) {
+            bool is_prime = true;
+            for (auto p : primes) {
+                is_prime = is_prime && (val % p != 0);
+            }
+            if (is_prime) {
+                primes.push_back(val);
+            }
+            val++;
+        }
+
+        return primes.back();
     }
 }  // namespace nth_prime
