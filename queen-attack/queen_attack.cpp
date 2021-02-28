40b5c5d9738469df0ce1718d71d5e8983f0cd6c1
JSanestrzik <j.sanestrzik@gmail.com>
2021-02-28 22:34:48 +0100

    Add Queen attack WIP solution

--- queen-attack/queen_attack.cpp
+++ queen-attack/queen_attack.cpp
@@ -1,5 +1,28 @@
 #include "queen_attack.h"
+#include <stdexcept>
 
 namespace queen_attack {
+    chess_board::chess_board(std::pair<int, int> white_queen_pos, std::pair<int, int> black_queen_pos):
+        m_white_queen_pos(white_queen_pos),
+        m_black_queen_pos(black_queen_pos) {
+        if (white_queen_pos == black_queen_pos) {
+            throw std::domain_error("Queen position values van't be the same");
+        }
+    }
 
+    const std::pair<int, int>& chess_board::white() const {
+        return m_white_queen_pos;
+    }
+
+    const std::pair<int, int>& chess_board::black() const {
+        return m_black_queen_pos;
+    }
+
+    bool chess_board::can_attack() const {
+        return true;
+    }
+
+    chess_board::operator std::string () const {
+        return "";
+    }
 }  // namespace queen_attack
--- queen-attack/queen_attack.h
+++ queen-attack/queen_attack.h
@@ -1,8 +1,24 @@
 #if !defined(QUEEN_ATTACK_H)
 #define QUEEN_ATTACK_H
+#include <utility>
+#include <string>
+#define  EXERCISM_RUN_ALL_TESTS
 
 namespace queen_attack {
+    class chess_board {
+    public:
+        chess_board(std::pair<int, int> white_queen_pos = {0, 3}, std::pair<int, int> black_queen_pos = { 7,3});
 
+        const std::pair<int, int>& white() const;
+        const std::pair<int, int>& black() const;
+        bool can_attack() const ;
+
+        operator std::string () const;
+    private:
+        const std::pair<int, int> m_white_queen_pos;
+        const std::pair<int, int> m_black_queen_pos;
+
+    };
 }  // namespace queen_attack
 
 #endif // QUEEN_ATTACK_H
