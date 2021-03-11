60f2f74bbac62f5b870bc6d7c80795a727758705
JSanestrzik <j.sanestrzik@gmail.com>
2021-03-11 23:48:12 +0100

    Update solution to pangram

--- pangram/pangram.cpp
+++ pangram/pangram.cpp
@@ -1,6 +1,7 @@
 #include "pangram.h"
 #include <set>
 #include <algorithm>
+#include <iostream>
 
 namespace pangram {
 
@@ -10,8 +11,9 @@ namespace pangram {
         }
 
         val.erase(std::remove_if(val.begin(), val.end(), [loc](char c){ return !std::isalpha(c, loc);}), val.end());
-        std::transform(val.begin(), val.end(), val.begin(), [](char c){ return std::tolower(c); });
-        const std::set<char> pangram(val.begin(), val.end());
-        return pangram.size() == alphabet_count;
+        std::transform(val.begin(), val.end(), val.begin(), [loc](char c){ return std::tolower(c, loc); });
+        std::sort(val.begin(), val.end());
+        val.erase(std::unique(val.begin(), val.end()), val.end());
+        return val.length() == alphabet_count;
     }
 }  // namespace pangram
--- pangram/pangram.h
+++ pangram/pangram.h
@@ -2,9 +2,10 @@
 #define PANGRAM_H
 #include <string>
 #include <locale>
+#define EXERCISM_RUN_ALL_TESTS
 
 namespace pangram {
-    bool is_pangram(std::string val, const std::size_t alphabet_count = 26, const std::locale loc = std::locale("en_US.UTF8"));
+    bool is_pangram(std::string val, const std::size_t alphabet_count = 26, const std::locale loc = std::locale("C"));
 }  // namespace pangram
 
 #endif // PANGRAM_H
